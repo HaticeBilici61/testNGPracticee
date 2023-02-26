b8eb3cc72c80ff4292f394b26b0b757960c8f8d7
Erol Evren <100449806+erlevren@users.noreply.github.com>
2023-02-26 19:37:21 +0300

    update

--- src/test/java/tests/Cross/CrossTest02.java
+++ src/test/java/tests/Cross/CrossTest02.java
@@ -6,7 +6,10 @@ import utilities.TestBaseCross;
 public class CrossTest02 extends TestBaseCross {
     @Test
     public void test02() {
+        extentTest=extentReports.createTest("2.Deneme Test");
         driver.get("https://techproeducation.com");
+        extentTest.info("Techpro adresine gidildi");
+        extentTest.pass("Test Passed");
 
 
     }
